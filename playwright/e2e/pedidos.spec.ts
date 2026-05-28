3ee7b8b9d01948462c0cccc5f3024a094f0410c6
Mariana Silva <marisil.tere@gmail.com>
2026-05-28 09:01:52 -0300

    aplicacao do padrao AAA

--- playwright/e2e/pedidos.spec.ts
+++ playwright/e2e/pedidos.spec.ts
@@ -1,20 +1,20 @@
 import { test, expect } from '@playwright/test'
 
+/// AAA - Arrange, Act, Assert
+/// PAV - Preparar, Agir, Verificar
+
 test('deve consultar um pedido aprovado', async ({ page }) => {
+    // Arrange
     await page.goto('http://localhost:5173/')
-
-    // Checkpoint 1: Verificar se a página de consulta de pedidos está carregada
     await expect(page.getByTestId('hero-section').getByRole('heading')).toContainText('Velô Sprint')
-
     await page.getByRole('link', { name: 'Consultar Pedido' }).click()
-
-    // Checkpoint 2: Verificar se a página de consulta de pedidos está carregada
     await expect(page.getByRole('heading')).toContainText('Consultar Pedido')
 
-    await page.getByTestId('search-order-id').click()
+    // Act
     await page.getByTestId('search-order-id').fill('VLO-X15UOQ')
-
     await page.getByTestId('search-order-button').click()
+    
+    // Assert
     await expect(page.getByTestId('order-result-id')).toBeVisible()
     await expect(page.getByTestId('order-result-id')).toContainText('VLO-X15UOQ')
     await expect(page.getByTestId('order-result-status')).toBeVisible()
